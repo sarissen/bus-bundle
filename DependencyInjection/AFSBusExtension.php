d63d491dedd803fa8af48a31dd4c050996b76da3
Sven Arissen <sven.arissen@beetree.nl>
2018-04-18 13:49:33 +0200

    Added Bundle Extension class to load services

---
+++ DependencyInjection/AFSBusExtension.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace AFS\BusBundle\DependencyInjection;
+
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+class AFSBusExtension extends Extension
+{
+
+    /**
+     * @inheritdoc
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__.'/../Resources/config')
+        );
+        $loader->load('services.yaml');
+    }
+
+}
